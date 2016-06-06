b72e6dbba03d896bc9f04b0c072d8759ed0f0cb1
michaeldelli-gatti <michaelddg@gmail.com>
2016-06-06 17:02:59 -0400

    login working

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,52 @@
 package com.theironyard;
 
+import spark.ModelAndView;
+import spark.Spark;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import spark.template.mustache.MustacheTemplateEngine;
+
+
 public class Main {
 
+    static User user;
+    static ArrayList<User> userlist = new ArrayList<>();
+    static ArrayList<String> messages = new ArrayList<>();
+    //static HashMap<String, String> userList = new HashMap<>();
+
     public static void main(String[] args) {
-	// write your code here
+        Spark.init();
+        Spark.get("/",
+                (request, response) -> {
+                    HashMap m = new HashMap();
+                    if (user == null) {
+                        return new ModelAndView(m, "index.html");
+                    } else {
+                        m.put("username", user.name);
+                        m.put("password", user.password);
+                        return new ModelAndView(m, "messages.html");
+                    }
+                },
+                new MustacheTemplateEngine()
+        );
+        Spark.post("/index",
+                (request, response) -> {
+                    String username = request.queryParams("username");
+                    String password = request.queryParams("password");
+                    user = new User(username, password);
+                    userlist.add(user);
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post("/messages",
+                (request, response) -> {
+                    String message = request.queryParams("message");
+                    messages.add(message);
+                    response.redirect("/");
+                    return "";
+                }
+                );
     }
 }
---
+++ src/com/theironyard/Message.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+/**
+ * Created by michaeldelli-gatti on 6/6/16.
+ */
+public class Message {
+    String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+}
