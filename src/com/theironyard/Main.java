68e17cdc9c86b846596b9d716f27da4e5b0714c2
michaeldelli-gatti <michaelddg@gmail.com>
2016-06-07 14:08:27 -0400

    add/delete messages. Also, the name finally shows up, so that's nice

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,6 +1,7 @@
 package com.theironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 
 import java.util.ArrayList;
@@ -10,22 +11,23 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 public class Main {
 
-    static User user;
-    //static ArrayList<User> userlist = new ArrayList<>();
-    static ArrayList<Message> messages = new ArrayList<>();
-    static HashMap<User, ArrayList<Message>> userList = new HashMap<>();
+    static HashMap<String, User> users = new HashMap<>();
 
     public static void main(String[] args) {
         Spark.init();
         Spark.get("/",
                 (request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("name");
+
                     HashMap m = new HashMap();
-                    if (user == null) {
+                    if (name == null) {
                         return new ModelAndView(m, "index.html");
                     } else {
-                        m.put("messages", messages);
-                        m.put("username", user.name);
-                        //m.put("password", user.password);
+                        User user = users.get(name);
+                        m.put("password", user.password);
+                        m.put("name", user.name);
+                        m.put("messages", user.messages);
                         return new ModelAndView(m, "messages.html");
                     }
                 },
@@ -33,19 +35,70 @@ public class Main {
         );
         Spark.post("/create-user",
                 (request, response) -> {
-                    String username = request.queryParams("username");
-                    String password = request.queryParams("password");
-                    user = new User(username, password);
+                    String name = request.queryParams("name");
+                    String pass = request.queryParams("password");
+                    if (name == null || pass == null){
+                        throw new Exception("name or pass not set");
+                    }
+
+                    User user = users.get(name);
+                    if (user == null){
+                        user = new User(name, pass);
+                        users.put(name, user);
+                    }
+                    else if (!pass.equals(user.password)){
+                        throw new Exception("wrong password");
+                    }
+
+                    Session session = request.session();
+                    session.attribute("name", name);
+
                     response.redirect("/");
                     return "";
                 }
         );
         Spark.post("/create-message",
                 (request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("name");
+                    if (name == null){
+                        throw new Exception("not logged in");
+                    }
+
                     String message = request.queryParams("message");
+                    if (message == null){
+                         throw new Exception("invalid message");
+                    }
+
+                    User user = users.get(name);
+                    if (user == null){
+                        throw new Exception("User does not exist");
+                    }
+
                     Message m = new Message(message);
-                    messages.add(m);
-                    //userList.put(user, messages);
+                    user.messages.add(m);
+
+                    response.redirect("/");
+                    return "";
+
+            }
+        );
+        Spark.post(
+                "/delete-message",
+                (request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("name");
+                    if (name == null) {
+                        throw new Exception("not logged in");
+                    }
+                    int id = Integer.valueOf(request.queryParams("id"));
+
+                    User user = users.get(name);
+                    if (id < 0 || id - 1 >= user.messages.size() ) {
+                        throw new Exception("invalid id");
+                    }
+                    user.messages.remove(id - 1);
+
                     response.redirect("/");
                     return "";
                 }
--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -1,11 +1,17 @@
 package com.theironyard;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
+
 /**
  * Created by michaeldelli-gatti on 6/6/16.
  */
 public class User {
     String name;
     String password;
+    ArrayList<Message> messages = new ArrayList<>();
+
+
 
     public User(String name, String password) {
         this.name = name;
